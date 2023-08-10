7a47b9bd16a712b0d378c94db6ddf90384f70236
balmed <iwymed@gamil.com>
2023-08-10 12:17:56 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*C program the function puts return 0
+*/
+
+int mian(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*C program the function printf return 0
+*/
+
+int mian(void)
+{
+	prinft("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
