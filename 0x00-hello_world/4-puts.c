636bd4236494f32df5680b8ea1cdf54e20a8a9f7
balmed <iwymed@gamil.com>
2023-08-10 17:11:21 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- *mian -Entry point
- *Desciption: A  program the function puts
- *Return: alawys 0 (Success)
+ *mian -  A  program that prints a line with puts fonction
+ *Return: 0 (Success)
 */
-
 int mian(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 
