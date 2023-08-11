92181b48d6f4cb8f1a5fa3467ccf9d94c7230cd2
balmed <iwymed@gamil.com>
2023-08-11 08:28:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 *Return:Always 0 (Success)
 */
 
-int mian(void)
+int main(void)
 {
 	
 
@@ -15,7 +15,7 @@ int mian(void)
         printf("size of a char: %lu byte(s)\n", sizeof(char)); 
         printf("size of a int: %lu byte(s)\n", sizeof(int));
         printf("size of a long long int: %lu byte(s)\n",sizeof(long long int));
-        printf("size of a float: %lu byte(s)", sizeof(float)); 
+        printf("size of a float: %lu byte(s)\n", sizeof(float)); 
         return (0);
 	
 }
