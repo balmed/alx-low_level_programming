90a70864ff3eed627465889cf0404d5ea604a543
balmed <iwymed@gamil.com>
2023-08-12 12:04:11 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,20 @@
 */
 int main(void)
 {
-        int i,j;
-        for (i=48;i<=56;i++)
-        {
-		for (j=49;i<=57;j++)
-                putchar(i);
-                if (j>i)
-                {
-                        putchar(i);
-                        putchar(j);
-			if (i !=56 || j !=57)
-			{
-				putchar('.');
-				putchar(' ');
-			}
-                }
-        }
-        putchar('\n');
-        return (0);
+int i, j;
+for (i = 48 ; i <= 56 ; i++)
+{
+for (j = 49 ; i <= 57 ; j++)
+putchar(i);
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+putchar('.');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
