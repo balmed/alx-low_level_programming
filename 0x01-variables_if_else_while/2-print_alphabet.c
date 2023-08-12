2410d61416cd0351f8f4713ccaddeca6cfae7973
balmed <iwymed@gamil.com>
2023-08-12 07:50:29 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-
 /**
-*main - Prints the alphabet
-*Return : Always 0 (Success)
+ * main - Entry point
+ * Description : print all aplphabet letters
+ * Return : Always 0 (Success)
 */
 int main(void)
 {
-	char alp[26]="abcdefghijklmnopqrstuwxyz";
-	int i;
-	for (i=0;i<26;i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+char alp[26] = "abcdefghijklmnopqrstuwxyz";
+int i;
+for (i = 0 ; i < 26 ; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
 }
