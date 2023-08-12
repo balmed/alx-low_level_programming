9f87b38398a146575bff4e2ac2a9fe20d647f6cc
balmed <iwymed@gamil.com>
2023-08-11 18:28:00 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,25 +3,24 @@
 #include <stdio.h>
 
 /**
-*main - Prints if number is positive, zero or negative
-*Return : Always 0 (Success)
-*/
+ * main - Prints if a number is positive, zero, or negative
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{
-	printf("%d is positive\n", n);
+        printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-	printf("%d is zero\n", n);
+        printf("%d is zero\n", n);
 }
 else
 {
-	printf("%d is negative\n", n);
+        printf("%d is negative\n", n);
 }
-	return (0);
-} 
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+*main - Prints the alphabet
+*Return : Always 0 (Success)
+*/
+int main(void)
+{
+        char alp[52]="abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+        for (i=0;i < 52;i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
+   
