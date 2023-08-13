cc3035af3906f44ce0140f3aa606c060744721ff
balmed <iwymed@gamil.com>
2023-08-12 20:12:21 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
 /**
-*main - Prints numbers form 0 to 9
-*Return : Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints numbers form 0 to 9 .
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i;
