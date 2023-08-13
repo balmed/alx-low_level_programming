1a721de79a0c2e4f9365262d0613d0d0647edf56
balmed <iwymed@gamil.com>
2023-08-13 10:51:07 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include<stdio>
+#include<stdio.h>
 #include <unistd.h>
 /**
 *main -Entry point
 *
 *Return:Always 1 (Success)
 */
-int mian(void)
+int main(void)
 {       
-    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,59);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",Stdout);
+return (1);
 }
