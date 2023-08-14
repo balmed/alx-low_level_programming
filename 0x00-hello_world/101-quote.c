bb72afd7a8bf94f55065f7b3cde75eae88a64f47
balmed <iwymed@gamil.com>
2023-08-13 19:46:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 /**
 *main -Entry point
 *
-*Return:Always 1 (Success)
+*Description:using sizeof to print ths sizeof various types.
+*
+*Return:Always 0 (Success)
 */
 int main(void)
-{       
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",Stdout);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
