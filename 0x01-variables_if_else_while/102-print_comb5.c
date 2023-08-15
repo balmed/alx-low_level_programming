b6d91b1a3c03bc0798bb72dd624ea6762b55bab3
balmed <iwymed@gamil.com>
2023-08-14 19:55:57 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
-
 /**
-*main - Prints all possible combinations of two tow digits 
-*Return : Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int i,j;
-        for (i=0;i< 100;i++)
-        {
-                for (j=0;j<=100;j++)
-                putchar(i);
-                if (j>i)
-                {
-                        putchar((i/10)+48);
-                        putchar((i%10)+48);
-			putchar(' ');
-			putchar((j/10)+48);
-			putchar((j/10)+48);
-                        if (i !=98 || j !=99)
-                        {
-                                putchar('.');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+int di1 = 0, di2;
+while (di1 <= 99)
+{di2 = di1;
+while (di2 <= 99)
+{
+if (di2 != di1)
+{
+putchar((di1 / 10) + 48);
+putchar((di1 % 10) + 48);
+putchar(' ');
+putchar((di2 / 10) + 48);
+putchar((di2 % 10) + 48);
+if (di1 != 98 || di2 != 99)
+{
+putchar(',');
+putchar(' ');
 }
+}
+di2++;
+}
+di1++;
+}
+putchar('\n');
+return (0);
+}
+
