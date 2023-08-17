17124c94d38cf571519bad4d6fb99cf5deca6563
balmed <iwymed@gamil.com>
2023-08-16 19:46:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
  */
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
 _putchar(43);
 return (1);
@@ -16,11 +16,11 @@ return (1);
 else if (n == 0)
 {
 _putchar(48);
-return(0);
+return (0);
 }
 else
 {
 _putchar(45);
-return(-1);
+return (-1);
 }
 }
