41795fad780e22e1da2632273e8f010ff62cc2fa
balmed <iwymed@gamil.com>
2023-08-17 09:55:50 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _putchar - multiplies two integers
+ * @a: The value to print
+ * @b: The vale to print
+ *
+ * Return: Result the two integers
+ */
+int mul(int a, int b)
+{
+int res = a * b;
+return (res);
+}
