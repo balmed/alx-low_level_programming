fe60ce790a3f0fc671d5a2492d844594315343dc
balmed <iwymed@gamil.com>
2023-08-18 11:00:14 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,15 @@
  */
 void print_number(int n)
 {
-unsigned int b = n;
+unsigned int b;
+b = n;
 if (n < 0)
 {
-n *= -1;
+n = n * -1;
 b = n;
 _putchar('_');
 }
-b /= 10;
+b = b / 10;
 if (b != 0)
 print_number(b);
 _putchar((unsigned int) n % 10 + '0');
