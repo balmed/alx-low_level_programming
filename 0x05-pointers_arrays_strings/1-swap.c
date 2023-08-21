2791583de3f9c27377885ef997bd542a003f1715
balmed <iwymed@gamil.com>
2023-08-21 08:45:27 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _putchar - swaps the values of two integers
+ * @a: input parameter 1
+ * @b: input parameter 1
+ *
+ * Return: empty
+ */
+void swap_int(int *a, int *b)
+{
+int t;
+t = *a;
+*a = *b;
+*b = t;
+}
