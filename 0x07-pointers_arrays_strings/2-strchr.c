f4cf2c315e03aeaa45d9641d81698d102f0cf035
balmed <iwymed@gamil.com>
2023-08-28 07:02:33 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - The length of a prefix substring.
+ * @s: String.
+ * @accept: Bytes.
+ *
+ * Return: Unisgned to the resulting (i).
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0 ; s[i] != '\0' ; i++)
+{
+for (j =0 ; accept[j] != s[i] ; j++)
+{
+if (accept[j] == '\0')
+return (i);
+}
+}
+return (i);
+}
+
+
