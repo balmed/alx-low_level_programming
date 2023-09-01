bf70e9481ed92a867fbf8967596f032e41bebd04
balmed <iwymed@gamil.com>
2023-09-01 15:07:14 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: int.
+ * @argv: list.
+ * @n: Maximum number of bytes from src.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
