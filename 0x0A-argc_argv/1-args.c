f24c6fb34522cb8ef00956189049f9575e2d19b6
balmed <iwymed@gamil.com>
2023-09-01 16:50:48 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,13 @@
 /**
  * main - prints the number of arguments passed into it.
  * @argc: numbre of arguments.
- * @argv: pointerp of  argumants.
+ * @argv: pointer of  argumants.
  *
  * Return:  Always 0 (Success).
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc , char *argv[])
 {
+(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
