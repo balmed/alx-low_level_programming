08010b500dfea553e4d9db7abc82889e7badb3d3
balmed <iwymed@gamil.com>
2023-09-11 16:13:04 -0700

    dog.h

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,20 +2,18 @@
 #include <stdio.h>
 /**
  * is_prime_number - check if n is a prime number
- * @oth: value int  
+ * @oth: value int
  * @n: value int 
  *
- * Return: On success 1.
- * On error, 0 .
- */
-int check_prime(int n, int oth)
+ * Return: 1 or 0 .
+ * */
+int check_prime(int n, int oth);
 int is_prime_number(int n)
 {
 return (check_prime(n, 2));
 }
-
 /**
- * check_prime - check all number < n if they can divide it 
+ * check_prime - check all number < n if they can divide it
  * @n: input int
  * @oth: input int 
  */
@@ -26,5 +24,5 @@ return (1);
 else if (n % oth == 0 || n <= 1)
 return (0);
 else
-return (check_prime(n + oth +1));
+return (check_prime(n, oth + 1));
 }
