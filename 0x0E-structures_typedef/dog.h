a9054636539ff9238eb62136ee6d3970673da19f
balmed <iwymed@gamil.com>
2023-09-11 16:07:38 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct of dog.
+ * @name: dog is name.
+ * @owner: dog is owner
+ * @age: dog is age.
+ *
+ * Description: Define type struct dog with the following elements.
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif
