e2b88e0a0ee1166c95c4469277f9d08448577c14
balmed <iwymed@gamil.com>
2023-09-13 15:07:32 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ *print_name - prints a name.
+ * @name: name of string.
+ * @f: prints by fonction pointer.
+ *
+ * Return: Empty.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+{
+f(name);
+}
+}
