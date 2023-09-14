2d6c1c8c2db68ab1616215d1d9a094bad443ce49
balmed <iwymed@gamil.com>
2023-09-13 17:42:04 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - addition tow numbres.
+ * @a: Numbre 1.
+ * @b: Numbre 2.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - subtraction tow numbres.
+ * @a: Numbre 1.
+ * @b: Numbre 2.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplication tow numbres.
+ * @a: Numbre 1.
+ * @b: Numbre 2.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - division tow numbres.
+ * @a: Numbre 1.
+ * @b: Numbre 2.
+ *
+ * Return: The division of a and b.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_div - modulo tow numbres.
+ * @a: Numbre 1.
+ * @b: Numbre 2.
+ *
+ * Return: The modulo of a and b.
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
