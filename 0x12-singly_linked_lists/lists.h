6c9333e259631ba5f68f06089befe885a4c64ca6
balmed <iwymed@gamil.com>
2023-09-21 16:50:41 -0700

    modify lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,19 +6,20 @@
 #include <string.h>
 
 /**
- * struct list_s - linked list.
- * @str: string - (malloc'ed string).
- * @len: lengh of the string.
- * @next: Points to next node.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: linked list node structure.
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
