22eff788ba911abdb146b1e29b2bb0dabe1e1fc4
balmed <iwymed@gamil.com>
2023-10-02 16:25:36 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,26 @@
 #include "main.h"
 #include <string.h>
-#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
- * main - program.
- * @ac: paramitre count.
- * @av: paramitre vector.
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of file.
+ * @text_content: text to write.
  *
- * Return: 1 on success 0 on failure.
+ * Return: 1 on success 0 on failure
  */
-int main(int ac, char **av)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t b;
-	char buf[READ_BUF_SIZE];
+	int fd;
+	ssize_t wrt = 0;
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	form_fd = open(av[1], O_RDONLY);
-		if (form_t == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to%s\n", av[2]), exit(99);
-	while ((b = read(form_fd, buf, ERAD_BUF_SIZE)) > 0)
-	if (write(to_fd, buf, b) != b)
-		dprintf(STDERR_FILENO, "Error: Can't write to%s\n", av[2]), exit(99);
-	if (b == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", av[1]), exit(98);
-	form_fd = close(form_fd);
-	to_fd = close(to_fd);
-	if (form_fd)
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE%d\n",
-				form_fd), exit(100);
-	if (to_fd)
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE%d\n",
-				form_fd), exit(100);
-	return (EXIT_SUCCESS);
+	ssize_t len = strlen(text_content);
 
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+	wrt = write(fd, text_content, len);
+	close(fd);
+	return (wrt == len ? 1 : -1);
 }
