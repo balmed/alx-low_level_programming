081d63f064f6bb7dc8d4fe69c25ecebf34d91a7c
balmed <iwymed@gamil.com>
2023-10-02 17:39:15 -0700

    crater funt strlen of 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,22 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: the string
+ *
+ * Return: return int.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: name of file.
@@ -12,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t wrt = 0;
 
-	ssize_t len = strlen(text_content);
+	ssize_t len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
