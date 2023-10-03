3ba3d2ca19a6968061a75295c7b2fff553e12470
balmed <iwymed@gamil.com>
2023-10-02 18:34:12 -0700

    modify2  3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,22 +19,22 @@ int main(int ac, char **av)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", av[1]), exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (file_to == -1)
-	dprintf(STDERR_FILENO, "Error: Can't write to%s\n", av[2]), exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((b = read(file_from, buf, READ_BUF_SIZE)) > 0)
 		if (write(file_to, buf, b) != b)
-			dprintf(STDERR_FILENO, "Error: Can't write to%s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	if (b == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", av[1]), exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from)
-	dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE%d\n",
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
 			file_from), exit(100);
 	if (file_to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE%d\n",
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
 				file_from), exit(100);
 	return (EXIT_SUCCESS);
 }
