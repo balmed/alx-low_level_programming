ee8fa791f00f82e0be4b566e7f9843a964b729bb
balmed <iwymed@gamil.com>
2023-11-13 17:59:01 -0800

    modify fils 101 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp  https://github.com/balmed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libbalm.so
+wget -P /tmp  https://github.com/balmed/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libbalm.so
 export LD_PRELOAD=/tmp/libbalm.so
