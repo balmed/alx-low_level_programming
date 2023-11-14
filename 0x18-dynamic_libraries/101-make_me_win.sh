0abd7624d11add2f20e454e3bd08a84d1c3b1faf
balmed <iwymed@gamil.com>
2023-11-13 17:46:28 -0800

    modify fils 101 tow

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp  https://github.com/balmed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.os
+wget -P /tmp  https://github.com/balmed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libbalm.so
+export LD_PRELOAD=/tmp/libbalm.so
---
+++ 0x18-dynamic_libraries/balm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * printf - print message.
+ * @format: Destination string.
+ * @...: Source string.
+ *
+ * Return: empty.
+ */
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 +9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
