c744995cb01c8f34944f27c25995ce52e964ab34
balmed <iwymed@gamil.com>
2023-11-30 17:09:33 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,35 @@
 #include "hash_tables.h"
+
+/**
+ * funt_hash_n - Create a new tables.
+ * @key: the node of key.
+ * @value: the node of value.
+ *
+ * Return: the new node; or NULL on failure.
+ */
+hash_node_t *funt_hash_n(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return NULL;
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return NULL;
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return NULL;
+	}
+	node->next = NULL;
+	return node;
+}
 /**
  * hash_table_set - set a kay a value the table.
  * @ht: hash table to add elemet.
@@ -10,7 +41,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_table_t *hash_node, *tmp;
+	hash_node_t *hash_node, *tmp;
 	char *new_value;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
@@ -31,7 +62,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	hash_node = make_hash_node(key, value);
+	hash_node = funt_hash_n(key, value);
 	if (hash_node == NULL)
 		return (0);
 	hash_node->next = ht->array[index];
